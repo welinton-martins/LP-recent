bc21aee84f0d1022a8f5bf3392b98873915d67c1
welinton-martins <welintonmartins4@outlook.com>
2021-05-28 14:17:45 -0300

    coping the task

---
+++ ArquivoDeTexto/ArquivosEx02.java
@@ -0,0 +1,13 @@
+public class ArquivosEx02 {
+	public static void main(String[] args) {
+		if (args.lenght != 4) {
+			System.out.println("Uso incorreto!");
+			System.exit(1);
+		}
+/*		try {
+			FileReader = 
+
+		}*/
+			
+	}
+}
