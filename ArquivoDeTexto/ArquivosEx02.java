58eddbfd10d238d32a4faf71fea1fad4a076744a
welinton-martins <welintonmartins4@outlook.com>
2021-05-28 14:26:48 -0300

    adding more copy

--- ArquivoDeTexto/ArquivosEx02.java
+++ ArquivoDeTexto/ArquivosEx02.java
@@ -1,13 +1,18 @@
+//import java.;
+
 public class ArquivosEx02 {
 	public static void main(String[] args) {
 		if (args.lenght != 4) {
 			System.out.println("Uso incorreto!");
 			System.exit(1);
 		}
-/*		try {
-			FileReader = 
-
-		}*/
+		try {
+			FileReader fr = new FileReader(args[0]);
+			FileWriter fw = new FileWriter(args[3]);
+			BufferedReader br = new BufferedReader(fr);
+			BufferedWriter bw = new BufferedWriter(fr);
 			
+		}
+	
 	}
 }
