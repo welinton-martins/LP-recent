c75b393d3114869f4146d5733f41c30f7bb28d76
welinton-martins <welintonmartins@outlook.com>
2021-05-31 14:30:54 -0300

    features to insert

--- TP_Imobiliaria/Imobiliaria.java
+++ TP_Imobiliaria/Imobiliaria.java
@@ -4,7 +4,26 @@ public class Imovel {
 	private int quartos;
 	private String bairro;
 	private float valor;
+	
 	public static void main(String[] args) {
+		
+	}
+	public void ImportarImoveis() {
+	
+	}
+	public void ExportarImoveis() {
+	
+	}	
+	public void MostrarDetalheDoImovel() {
+
+	}
+	public void InserirNovoImovel() {
+		
+	}
+	public void RemoverImovel() {
+		
+	}	
+	public void Sair() {
 		
 	}
 }
