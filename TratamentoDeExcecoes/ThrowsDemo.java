d17bcd570a0ca6f0db7e8d40c0b29a41119d8b4b
welinton-martins <welintonmartins4@outlook.com>
2021-06-02 00:44:19 +0000

    throws exeption

---
+++ TratamentoDeExcecoes/ThrowsDemo.java
@@ -0,0 +1,14 @@
+class ThrowsDemo {
+    static void throwOne () throws IllegalAccessException  {
+        System.out.println("Dentro de throwOne.");
+        throw new IllegalAccessException("demo");
+    }
+
+    public static void main(String args[]) {
+        try {
+            throwOne();
+        } catch (IllegalAccessException e) {
+            System.out.println("Pego" + e);
+        }
+    }
+}
